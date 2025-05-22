926220e8c643cb4279ab5f56029c63b1f1673cdc
Hun <adeliae.p1841@gmail.com>
2025-05-22 19:55:07 +0900

    [BE] 유저 인증 관련 구현
    - 회원가입 구현
    - 로그인 구현
    - 로그아웃 구현
    - CORS 문제 해결
    
    #2

--- backend/flix_n_chill/urls.py
+++ backend/flix_n_chill/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/movies/', include('movies.urls')),
+    path('accounts/', include('dj_rest_auth.urls')),
+    path('accounts/registration/', include('dj_rest_auth.registration.urls'))
 ]
