1a39f65aeb75ef70ca37ac17e9f718861e976365
Hun-Hong <adeliae.p1841@gmail.com>
2025-05-23 14:36:12 +0900

    [BE] 상세 정보 provider 추가
    - movie provider 정보를 상세정보에 추가
    - 기본 fixture 약 460개 수집
    
    #6

--- backend/movies/serializers.py
+++ backend/movies/serializers.py
@@ -7,12 +7,25 @@ class MovieListSerializer(serializers.ModelSerializer):
         fields = ("id", "vote_average", "poster_path", "release_date", "genres", "title", "original_title")
         read_only_field = ("genres")
 
-class MovieDetailSerializer(serializers.ModelSerializer):
+class MovieCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = "__all__"
         read_only_field = ("rating", "genres")
 
+
+class MovieDetailSerializer(serializers.ModelSerializer):
+    class ProviderSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = MovieProvider
+            fields = "__all__"
+
+    providers = ProviderSerializer(many=True)
+
+    class Meta:
+        model = Movie
+        fields = "__all__"
+        read_only_field = ("rating", "genres")
 
 
 class ProviderSerilizer(serializers.ModelSerializer):
