8738f1c6a0118f2f664ff562d034e864ef8c02c3
Hun-Hong <adeliae.p1841@gmail.com>
2025-05-25 01:48:32 +0900

    [BE] 장르별, 정렬, 필터 구현
    - 정렬 API 구현
    - 필터 API 구현
    
    #32

--- backend/movies/views.py
+++ backend/movies/views.py
@@ -11,6 +11,7 @@ from rest_framework.exceptions import ValidationError
 from rest_framework.decorators import api_view
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from django.db.models import Q
+from datetime import datetime
 
 # Create your views here.
 
@@ -119,10 +120,30 @@ class MovieListView(ListAPIView):
     
     def get_queryset(self):
         ordering = self.request.query_params.get('ordering', 'latest')
+        year = self.request.query_params.get('year', None)
+
+        queryset = Movie.objects.all()
+
+        # 연도 필터
+        if year:
+            try:
+                year = int(year)
+                start_date = datetime(year, 1, 1)
+                end_date = datetime(year, 12, 31)
+                queryset = queryset.filter(release_date__range=(start_date, end_date))
+            except ValueError:
+                pass  # year가 숫자가 아닌 경우 무시
+
         if ordering == 'top':
-            return Movie.objects.order_by("-vote_average")
+            queryset = queryset.order_by("-vote_average")
+        elif ordering == 'bottom':
+            queryset = queryset.order_by("vote_average")
+        elif ordering == 'oldest':
+            queryset = queryset.order_by("release_date")
         else:
-            return Movie.objects.order_by("-release_date")
+            queryset = queryset.order_by("-release_date")
+
+        return queryset
 
 
 class MovieDetailView(RetrieveAPIView):
@@ -135,22 +156,49 @@ class MovieGenreListView(ListAPIView):
 
     def get_queryset(self):
         name_to_id = {
-        "action": 1,
-        "comedy": 4,
-        "drama": 7,
-        "horror": 11,
-        "adventure": 2,
-        "family": 8,
-        "romance": 14,
+            "action": 1,
+            "comedy": 4,
+            "drama": 7,
+            "horror": 11,
+            "adventure": 2,
+            "family": 8,
+            "romance": 14,
         }
+
         genre_name = self.kwargs.get("genre_name")
         ordering = self.request.query_params.get('ordering', 'latest')
-        genre = Genre.objects.get(id=name_to_id[genre_name])
-        
+        year = self.request.query_params.get('year', None)
+
+        # 장르 존재 확인
+        genre_id = name_to_id.get(genre_name)
+        if genre_id is None:
+            return Movie.objects.none()
+
+        genre = Genre.objects.get(id=genre_id)
+
+        queryset = Movie.objects.filter(genres=genre)
+
+        # 연도 필터링
+        if year:
+            try:
+                year = int(year)
+                start_date = datetime(year, 1, 1)
+                end_date = datetime(year, 12, 31)
+                queryset = queryset.filter(release_date__range=(start_date, end_date))
+            except ValueError:
+                pass  # 숫자가 아닌 경우 무시
+
+        # 정렬
         if ordering == 'top':
-            return Movie.objects.filter(genres=genre).order_by("-vote_average")
+            queryset = queryset.order_by("-vote_average")
+        elif ordering == 'bottom':
+            queryset = queryset.order_by("vote_average")
+        elif ordering == 'oldest':
+            queryset = queryset.order_by("release_date")
         else:
-            return Movie.objects.filter(genres=genre).order_by("-release_date")
+            queryset = queryset.order_by("-release_date")
+
+        return queryset
 
 
 class MovieSearchView(ListAPIView):
